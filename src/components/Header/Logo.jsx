8c2b0579fd8c1f7773468a40a0a39423b3917719
Felipe Coelho <felipeaacoelho.com>
2024-11-01 17:43:11 -0300

    merge

--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -1,8 +1,12 @@
 import styles from "./Logo.module.css";
+// import logo from "../../assets/logo3.png";
+
 export function Logo() {
+  const logo = "../../assets/logo3.png"
+ 
   return (
     <div className={styles.logo}>
-      <img src="../../assets/logo3.png" />
+      <img src={logo}/>
       <strong>Reservas</strong>
     </div>
   );
